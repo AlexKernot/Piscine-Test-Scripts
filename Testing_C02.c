255f677c6aada54fe9fe6ea8fc858166a150b744
Alex Kernot <alex.kernot04@gmail.com>
2023-02-08 08:54:24 +1030

    Update Testing_C02 to include all exercises

--- Testing_C02.c
+++ Testing_C02.c
@@ -6,41 +6,46 @@
 /*   By: akernot <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/31 11:33:53 by akernot           #+#    #+#             */
-/*   Updated: 2023/02/02 10:10:10 by akernot          ###   ########.fr       */
+/*   Updated: 2023/02/04 11:36:29 by akernot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <unistd.h>
 
-extern char *ft_strcpy(char *dest, char *src);
-extern char *ft_strncpy(char *dest, char *src, unsigned int n);
-extern int	ft_str_is_alpha(char *str);
-extern int ft_str_is_numeric(char *str);
-extern int	ft_str_is_lowercase(char *str);
-extern int ft_str_is_uppercase(char *str);
-extern int ft_str_is_printable(char *str);
-extern char	*ft_strupcase(char *str);
-extern char *ft_strlowcase(char *str);
-extern char *ft_strcapitalize(char *str);
+char *ft_strcpy(char *dest, char *src);
+char *ft_strncpy(char *dest, char *src, unsigned int n);
+int	ft_str_is_alpha(char *str);
+int ft_str_is_numeric(char *str);
+int	ft_str_is_lowercase(char *str);
+int ft_str_is_uppercase(char *str);
+int ft_str_is_printable(char *str);
+char	*ft_strupcase(char *str);
+char *ft_strlowcase(char *str);
+char *ft_strcapitalize(char *str);
 unsigned int ft_strlcpy(char *dest, char *src, unsigned int size);
 void ft_putstr_non_printable(char *str);
+void *ft_print_memory(void *addr, unsigned int size);
 
 int main(void)
 {
 	//	EX00
-	char src[5] = "TEST";
-	char dest[5] = "NULL";
+	printf("%s", "\n\n-------ex00-------\n");
+	char src[] = "0000TEST";
+	char dest[] = "00NULL0000";
 
 	ft_strcpy(dest, src);
 
 	printf("%s\n", dest);
 
 	//	EX01
-	ft_strncpy(dest, src, 5);
+	printf("%s", "\n\n-------ex01-------\n");
+	ft_strncpy(dest, src, 3);
 	printf("%s\n", dest);
 
 	//	EX02
-	printf("%d", ft_str_is_alpha("TEST"));
+	printf("%s", "\n\n-------ex02-------\n");
+	printf("%d\n", ft_str_is_alpha("TEST"));
 	printf("%d\n", ft_str_is_alpha("[FAIL"));
 	printf("%d\n", ft_str_is_alpha("0FAIL")); 
 
@@ -78,7 +83,8 @@ int main(void)
 	printf("%s\n", ft_strlowcase(&string[0]));
 
 	//	Ex09
-	char string2[24] = "capitalize+this wo-rd";
+		printf("%s", "\n\n-------ex09-------\n");
+	char string2[] = "capitalize+this wo-rd{a},m+i%sas";
 	printf("%s\n", ft_strcapitalize(&string2[0]));
 
 	//	Ex10
@@ -87,6 +93,11 @@ int main(void)
 	printf("%s\n", string2);
 
 	//	Ex11
-	printf("%s", "\n\n-------ex11-------\n");
-	ft_putstr_non_printable("Test\n");
+	write(1, "\n\n-------ex11-------\n", 21);
+	ft_putstr_non_printable("\x0a \x8e \x88\xfc\xa0+\xc2+");
+
+	//	Ex12
+	write(1, "\n\n-------ex12-------\n", 21);
+	char string3[] = "This this a literal and I need this to be 100 charactes, I don't know how many this is but it's certainly a few. \5\3\6 How. many now?:.,mnb";
+	ft_print_memory(&string3[0], sizeof(string3) / sizeof(char));
 }
